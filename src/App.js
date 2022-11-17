02c37ca485e558f410f453aeee5fed5b43606b0e
Dje Par <jparadis21@hotmail.com>
2022-11-18 00:40:28 +0700

    Week 1 Exercices

--- src/App.js
+++ src/App.js
@@ -1,41 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
-const data = [
-  {
-    id: "1",
-    title: "Tiramisu",
-    description : "The best tiramisu in Town",
-    image: "https://picsum.photos/200/300/?random",
-    price : "5.00",
-  },
-  {
-    id: "2",
-    title: "Lemon Ice Cream",
-    description : "The best Ice Cream in the World",
-    image: "https://picsum.photos/200/300/?random",
-    price : "50.00",
-  },{
-    id: "3",
-    title: "Chocolate mousse",
-    description : "The best Chocolate mousse in the Universe",
-    image: "https://picsum.photos/200/300/?random",
-    price : "500.00",
-  }
 
-]
-
-const topDesserts = data.map(dessert => {
-  return {
-    content : `${dessert.title} - | ${dessert.description}`,
-    price : dessert.price,
-  }
-})
 function App() {
-  console.log(topDesserts)
+
+
   return (
     <div>
-      <h1>Examine the console log</h1>
+      <h1>Hello World</h1>
     </div>
   );
 }
---
+++ src/components/Week1/ListMapping.js
@@ -0,0 +1,43 @@
+
+const data = [
+  {
+    id: "1",
+    title: "Tiramisu",
+    description : "The best tiramisu in Town",
+    image: "https://picsum.photos/200/300/?random",
+    price : "5.00",
+  },
+  {
+    id: "2",
+    title: "Lemon Ice Cream",
+    description : "The best Ice Cream in the World",
+    image: "https://picsum.photos/200/300/?random",
+    price : "50.00",
+  },{
+    id: "3",
+    title: "Chocolate mousse",
+    description : "The best Chocolate mousse in the Universe",
+    image: "https://picsum.photos/200/300/?random",
+    price : "500.00",
+  }
+
+]
+
+
+
+function ListMapping() {
+  const listItems = data.map(dessert => {
+    const itemText = `${dessert.title} - ${dessert.price}`
+    return <li>{itemText}</li>
+  })
+
+  return (
+    <div>
+      <ul>
+        {listItems}
+      </ul>
+    </div>
+  );
+}
+
+export default ListMapping;
